bbb93887c326cd35eee8f3aaf8718533fec8e71a
Godson Jerry <jegoisgs66@gmail.com>
2025-11-16 06:31:07 +0100

    added error handling

--- src/pages/services/api.js
+++ src/pages/services/api.js
@@ -1,10 +1,11 @@
 // the api here
 import axios from "axios";
+import { toast } from "react-toastify";
+
 const api = axios.create({
   baseURL: "https://daily-hustle-backend-fb9c10f98583.herokuapp.com/api/v1",
   timeout: 60000, // 60 seconds
 });
-
 
 // Add a request interceptor
 api.interceptors.request.use(
@@ -16,6 +17,17 @@ api.interceptors.request.use(
     return config;
   },
   (error) => Promise.reject(error)
+);
+
+// Add a response interceptor for error handling
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.data?.message) {
+      toast.error(error.response.data.message);
+    }
+    return Promise.reject(error);
+  }
 );
 
 export default api;
